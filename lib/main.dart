26e9079ca6a9508cbfa940847da3fff43271f847
Ikhvan <ikhvankanaya123@gmail.com>
2024-03-20 20:14:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:dicoding_wisatabandung/main_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: const MainScreen(),
+    );
+  }
+}
